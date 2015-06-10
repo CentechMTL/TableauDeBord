8c135d5042c70d9f9914a68a4beca682dbe3646d
RignonNoel <rignon.noel@openmailbox.org>
2015-06-10 13:56:05 -0400

    Traduction of KPI app

--- app/kpi/models.py
+++ app/kpi/models.py
@@ -14,19 +14,25 @@ KPI_TYPE_CHOICES = (
 
 #Type of kpi (ex: IRL)
 class KpiType(models.Model):
-    name = models.CharField(max_length=50)
+    class Meta:
+        verbose_name_plural = _('Types of KPI')
+
+    name = models.CharField(max_length=50, verbose_name=_('Name'))
 
     def __str__(self):
         return self.name
 
 #KPIs
 class KPI(models.Model):
-    company = models.ForeignKey(Company)
-    phase = models.ForeignKey(CompanyStatus)
-    type = models.ForeignKey(KpiType)
-    level = models.IntegerField(choices=((0, 0),(1, 1),(2, 2),(3, 3),(4, 4),(5, 5),(6, 6),(7, 7),(8, 8),(9, 9)))
-    period_start = models.DateField(auto_now_add=True, auto_now=False,)
-    comment = models.TextField(blank=True, default="")
+    class Meta:
+        verbose_name_plural = _('KPIs')
+
+    company = models.ForeignKey(Company, verbose_name=_('Companies'))
+    phase = models.ForeignKey(CompanyStatus, verbose_name=_('Status'))
+    type = models.ForeignKey(KpiType, verbose_name=_('Type of KPI'))
+    level = models.IntegerField(choices=((0, 0),(1, 1),(2, 2),(3, 3),(4, 4),(5, 5),(6, 6),(7, 7),(8, 8),(9, 9)), verbose_name=_('Level'))
+    period_start = models.DateField(auto_now_add=True, auto_now=False, verbose_name=_('Date'))
+    comment = models.TextField(blank=True, default="", verbose_name=_('Comment'))
 
     def __str__(self):
         return self.company.name

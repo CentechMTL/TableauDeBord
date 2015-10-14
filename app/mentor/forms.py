27156ffbff055de8bbba89bbefe824f34e4bb0ef
RignonNoel <rignon.noel@openmailbox.org>
2015-10-14 09:23:22 -0400

    Refactoring mentor form

--- app/mentor/forms.py
+++ app/mentor/forms.py
@@ -17,6 +17,7 @@ from django.utils.translation import ugettext_lazy as _
 FILTER_MENTOR_CHOICES = list(MENTOR_TYPE_CHOICES)
 FILTER_MENTOR_CHOICES.insert(0, ('','---------') )
 
+
 class MentorFilter(FilterSet):
     name = MethodFilter(action='filter_username', label=_('Search by name'))
     type = ChoiceFilter(choices= FILTER_MENTOR_CHOICES )
@@ -50,10 +51,15 @@ class MentorFilter(FilterSet):
         else:
             return queryset
 
-class MentorCreateForm(forms.Form):
+
+class MentorForm(forms.ModelForm):
+    class Meta:
+        model = Mentor
+        fields = ['type', 'url', 'picture', 'about', 'expertise', 'phone', 'website', 'facebook', 'twitter',
+                  'googlePlus', 'linkedIn']
 
     type = forms.ChoiceField(
-        choices = MENTOR_TYPE_CHOICES,
+        choices=MENTOR_TYPE_CHOICES,
     )
 
     url = forms.URLField(
@@ -85,30 +91,6 @@ class MentorCreateForm(forms.Form):
         required=False,
     )
     linkedIn.widget.attrs.update({'placeholder': _(u'https://ca.linkedin.com/in/username')})
-
-    firstname = forms.CharField(
-        label=_('First name'),
-        required=True,
-    )
-    firstname.widget.attrs.update({'placeholder': _(u'First name')})
-
-    lastname = forms.CharField(
-        label=_('Last name'),
-        required=True,
-    )
-    lastname.widget.attrs.update({'placeholder': _(u'Last name')})
-
-    username = forms.CharField(
-        label=_('Username'),
-        required=True,
-    )
-    username.widget.attrs.update({'placeholder': _(u'Username')})
-
-    email = forms.CharField(
-        label=_('Email'),
-        required=True,
-    )
-    email.widget.attrs.update({'placeholder': _(u'Email')})
 
     phone = forms.CharField(
         label=_('Phone number'),
@@ -145,165 +127,3 @@ class MentorCreateForm(forms.Form):
         label=_('Picture'),
         required=False,
     )
-
-    def __init__(self, *args, **kwargs):
-        super(MentorCreateForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_class = 'content-wrapper'
-        self.helper.form_method = 'post'
-
-        self.helper.layout = Layout(
-            HTML("<h1>"),
-            HTML(_("New mentor")),
-            HTML("</h1>"),
-            Field('firstname'),
-            Field('lastname'),
-            Field('username'),
-            Field('email'),
-            Field('picture'),
-            Field('phone'),
-            Field('website'),
-            Field('url'),
-            Field('facebook'),
-            Field('twitter'),
-            Field('googlePlus'),
-            Field('linkedIn'),
-            Field('type'),
-            Field('expertise'),
-            HTML(_(u"""Use the Ctrl key on your keyboard to select multiple. <br> If it lacks a domain that you would like to add to this list, do not hesitate to talk to the team Centech""")),
-            Field('about'),
-            StrictButton(_('Save'), type="submit")
-        )
-
-class MentorUpdateForm(forms.Form):
-
-    type = forms.ChoiceField(
-        choices = MENTOR_TYPE_CHOICES,
-    )
-
-    url = forms.URLField(
-        label=_('Directory of ETS'),
-        required=False,
-    )
-    url.widget.attrs.update({'placeholder': _(u'https://example.com')})
-
-    facebook = forms.URLField(
-        label=_('Facebook'),
-        required=False,
-    )
-    facebook.widget.attrs.update({'placeholder': _(u'https://www.facebook.com/lastname.firstname')})
-
-    twitter = forms.URLField(
-        label=_('Twitter'),
-        required=False,
-    )
-    twitter.widget.attrs.update({'placeholder': _(u'https://twitter.com/username')})
-
-    googlePlus = forms.URLField(
-        label=_('Google+'),
-        required=False,
-    )
-    googlePlus.widget.attrs.update({'placeholder': _(u'https://plus.google.com/id')})
-
-    linkedIn = forms.URLField(
-        label=_('linkedIn'),
-        required=False,
-    )
-    linkedIn.widget.attrs.update({'placeholder': _(u'https://ca.linkedin.com/in/username')})
-
-    firstname = forms.CharField(
-        label=_('First name'),
-        required=False,
-    )
-    firstname.widget.attrs.update({'placeholder': _(u'First name')})
-
-    lastname = forms.CharField(
-        label=_('Last name'),
-        required=False,
-    )
-    lastname.widget.attrs.update({'placeholder': _(u'Last name')})
-
-    phone = forms.CharField(
-        label=_('Phone number'),
-        required=False,
-        max_length=10
-    )
-    phone.widget.attrs.update({'placeholder': _(u'Phone number')})
-
-    website = forms.URLField(
-        label=_('Web site'),
-        required=False,
-    )
-    website.widget.attrs.update({'placeholder': _(u'https://example.com')})
-
-    about = forms.CharField(
-        label=_('Description'),
-        required=False,
-        widget=forms.Textarea(
-            attrs={
-                'placeholder': _(u'Write here a brief summary of your skills and your career.'),
-                'class': 'md-editor'
-            }
-        )
-    )
-
-    expertise = forms.ModelMultipleChoiceField(
-        label=_(u"Areas of expertise"),
-        queryset=Expertise.objects.all(),
-        required=False,
-        widget=forms.SelectMultiple(
-            attrs={
-                'required': 'required',
-            }
-        )
-    )
-
-    picture = forms.ImageField(
-        label=_('Picture'),
-        required=False,
-    )
-
-    def __init__(self, mentor, *args, **kwargs):
-        super(MentorUpdateForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_class = 'content-wrapper'
-        self.helper.form_method = 'post'
-
-        self.mentor = mentor
-        self.fields['about'].initial = mentor.about
-        self.fields['expertise'].initial = mentor.expertise.all
-        self.fields['firstname'].initial = mentor.user.first_name
-        self.fields['lastname'].initial = mentor.user.last_name
-        self.fields['phone'].initial = mentor.phone
-        self.fields['website'].initial = mentor.website
-        self.fields['facebook'].initial = mentor.facebook
-        self.fields['twitter'].initial = mentor.twitter
-        self.fields['googlePlus'].initial = mentor.googlePlus
-        self.fields['linkedIn'].initial = mentor.linkedIn
-        self.fields['type'].initial = mentor.type
-        self.fields['url'].initial = mentor.url
-
-        self.helper.layout = Layout(
-            HTML("<h1>"),
-            HTML(_("Update this mentor")),
-            HTML("</h1>"),
-            Field('firstname'),
-            Field('lastname'),
-            Field('picture'),
-            Field('phone'),
-            Field('website'),
-            Field('facebook'),
-            Field('twitter'),
-            Field('googlePlus'),
-            Field('linkedIn'),
-            Field('url'),
-            Field('type'),
-            Field('expertise'),
-            HTML(_(u"""Use the Ctrl key on your keyboard to select multiple. <br> If it lacks a domain that you would like to add to this list, do not hesitate to talk to the team Centech""")),
-            Field('about'),
-            StrictButton(_('Save'), type="submit")
-        )
-
-    def save(self):
-        self.mentor.save()
-        self.mentor.user.save()
--- app/mentor/views.py
+++ app/mentor/views.py
@@ -1,138 +1,50 @@
 # coding: utf-8
 
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, render_to_response, get_object_or_404
-from django.http import JsonResponse
+from django.http import HttpResponseRedirect
 from django.views import generic
 
 from django.contrib.auth.models import User
 from app.mentor.models import Mentor
-from app.home.models import Education, Expertise
 from app.company.models import Company
 
-from app.mentor.forms import MentorFilter, MentorUpdateForm, MentorCreateForm
-
+from app.mentor.forms import MentorFilter, MentorForm
+from app.home.forms import UserForm
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from django.core.urlresolvers import reverse_lazy, reverse
-from django.contrib import messages
-from django.conf import settings
-from django.core.mail import EmailMessage
-import random
-import os
+from django.core.urlresolvers import reverse_lazy
+
 
 class MentorCreate(generic.CreateView):
-    model = Mentor
+    model = User
     template_name = 'mentor/mentor_form.html'
-    form_class = MentorCreateForm
-    mentorCreate = None
+    form_class = UserForm
 
-    #You need to be connected, and you need to have access as centech only
+    # You need to be connected, and you need to have access as centech only
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         if self.request.user.profile.isCentech():
             return super(MentorCreate, self).dispatch(*args, **kwargs)
 
-        #The visitor can't see this page!
+        # The visitor can't see this page!
         return HttpResponseRedirect("/user/noAccessPermissions")
 
-    def get_form(self, form_class):
-        form = form_class()
-
-        return form
-
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST, request.FILES)
-        if form.is_valid():
-            self.create_mentor(form)
-            return self.form_valid(form)
-
-        return render(request, self.template_name, {'form': form})
-
-    def form_valid(self, form):
-        return HttpResponseRedirect(self.get_success_url())
-
-    def create_mentor(self, form):
-        lastname = form.data['lastname']
-        firstname = form.data['firstname']
-        username = form.data['username']
-        email = form.data['email']
-        newUser = User(username=username)
-
-        caractere = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRST0123456789"
-        password = ""
-        for index in range(10):
-            password += random.choice(caractere)
-
-        newUser.set_password(password)
-        newUser.save()
-        newUser.first_name = firstname
-        newUser.last_name = lastname
-        newUser.email = email
-        newUser.save()
-
-        newMentor = Mentor(user = newUser)
-        newMentor.save()
-        self.mentorCreate= newMentor
-        newMentor.phone = form.data['phone']
-        newMentor.website = form.data['website']
-        newMentor.facebook = form.data['facebook']
-        newMentor.twitter = form.data['twitter']
-        newMentor.googlePlus = form.data['googlePlus']
-        newMentor.linkedIn = form.data['linkedIn']
-        newMentor.about = form.data['about']
-        newMentor.type = form.data['type']
-        newMentor.url = form.data['url']
-
-        try:
-            self.request.FILES['picture'].name = newMentor.user.username + os.path.splitext(self.request.FILES['picture'].name)[1]
-            newMentor.picture = self.request.FILES['picture']
-        except:
-            pass
-
-        for expertise in form.cleaned_data["expertise"]:
-                newMentor.expertise.add(expertise)
-
-        newMentor.save()
-
-        app = settings.DASHBOARD_APP
-        message = u"Un compte vient de vous être créé pour accéder au Tableau de Bord du Centech. Vous pouvez dès maintenant vous y connecter avec les identifiants ci-dessous : \n\n"
-        message += u"Username : "
-        message += username
-        message += u"\n"
-        message += u"Password : "
-        message += password
-        message += u"\n\n\n"
-        message += u"Pour toute question ou demande d'aide, n'hésitais pas à nous contacter à l'adresse suivante : "
-        message += app['site']['email_technique']
-        message += u"\n\n"
-        message += u"Nous vous souhaitons une agréable journée!"
-        message += u"\n\n"
-        message += u"----------------------------------------\n\n"
-        message += u"Ce message du Centech est un élément important d'un programme auquel vous ou votre entreprise participer. Si ce n'est pas le cas veuillez nous en excuser et effacer ce message.\n"
-        message += u"Si nous persistons à vous envoyer des courriel sans votre accord, contacter nous à l'adresse suivante : "
-        message += app['site']['email_technique']
-
-        emailReady = EmailMessage('Bienvenue sur le Tableau de Bord du Centech', message, app['site']['email_technique'],
-            [email], [app['site']['email_technique']],
-            reply_to=[app['site']['email_technique']])
-        emailReady.send(fail_silently=False)
-
     def get_success_url(self):
-        return reverse_lazy("mentor:detail", kwargs={'pk': self.mentorCreate.userProfile_id})
+        mentor = Mentor.objects.create(user=self.object)
+        return reverse_lazy("mentor:detail", kwargs={'pk': mentor.userProfile_id})
 
-#Form for update profile of a mentor
+
+# Form for update profile of a mentor
 class MentorUpdate(generic.UpdateView):
     model = Mentor
-    form_class = MentorUpdateForm
+    form_class = MentorForm
 
-    #You need to be connected, and you need to have access as founder or centech
+    # You need to be connected, and you need to have access as founder or centech
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
-        #For know the company of the user if is a founder
+        # For know the company of the user if is a founder
         if self.request.user.profile.isMentor():
             try:
-                if(int(self.request.user.profile.userProfile_id) == int(self.kwargs['pk'])):
+                if int(self.request.user.profile.userProfile_id) == int(self.kwargs['pk']):
                     return super(MentorUpdate, self).dispatch(*args, **kwargs)
             except:
                 pass
@@ -140,57 +52,15 @@ class MentorUpdate(generic.UpdateView):
         if self.request.user.profile.isCentech():
             return super(MentorUpdate, self).dispatch(*args, **kwargs)
 
-        #The visitor can't see this page!
+        # The visitor can't see this page!
         return HttpResponseRedirect("/user/noAccessPermissions")
-
-    def get_form(self, form_class):
-        mentor = self.get_object()
-        form = form_class(mentor)
-
-        return form
-
-    def get_object(self, queryset=None):
-        return get_object_or_404(Mentor, userProfile_id=self.kwargs['pk'])
-
-    def post(self, request, *args, **kwargs):
-        object = self.get_object()
-        form = self.form_class(object, request.POST, request.FILES)
-
-        if form.is_valid():
-            self.update_user(object, form)
-            return self.form_valid(form)
-
-        return render(request, self.template_name, {'form': form})
-
-    def update_user(self, object, form):
-        object.user.last_name = form.data['lastname']
-        object.user.first_name = form.data['firstname']
-
-        object.phone = form.data['phone']
-        object.website = form.data['website']
-        object.facebook = form.data['facebook']
-        object.twitter = form.data['twitter']
-        object.googlePlus = form.data['googlePlus']
-        object.linkedIn = form.data['linkedIn']
-        object.about = form.data['about']
-        object.type = form.data['type']
-        object.url = form.data['url']
-
-        try:
-            self.request.FILES['picture'].name = object.user.username + os.path.splitext(self.request.FILES['picture'].name)[1]
-            object.picture = self.request.FILES['picture']
-        except:
-            pass
-
-        object.expertise.clear()
-        for expertise in form.cleaned_data["expertise"]:
-                object.expertise.add(expertise)
 
     def get_success_url(self):
         return reverse_lazy("mentor:detail", kwargs={'pk': self.kwargs['pk']})
 
-#List of all mentors
+
 class MentorIndex(generic.ListView):
+    # List of all mentors
     template_name = 'mentor/index.html'
     context_object_name = 'mentors'
 
@@ -208,10 +78,10 @@ class MentorIndex(generic.ListView):
         if self.request.user.profile.isExecutive():
             return super(MentorIndex, self).dispatch(*args, **kwargs)
 
-        #The visitor can't see this page!
+        # The visitor can't see this page!
         return HttpResponseRedirect("/user/noAccessPermissions")
+
     def get_queryset(self):
-        obj = Mentor.objects.all()
         return Mentor.objects.all()
 
     def get_context_data(self, **kwargs):
@@ -220,8 +90,9 @@ class MentorIndex(generic.ListView):
         context['mentorFilter'] = mf
         return context
 
-#Display detail of a mentor
+
 class MentorView(generic.DetailView):
+    # Display detail of a mentor
     model = Mentor
     template_name = 'mentor/detail.html'
 
@@ -239,7 +110,7 @@ class MentorView(generic.DetailView):
         if self.request.user.profile.isExecutive():
             return super(MentorView, self).dispatch(*args, **kwargs)
 
-        #The visitor can't see this page!
+        # The visitor can't see this page!
         return HttpResponseRedirect("/user/noAccessPermissions")
 
     def get_context_data(self, **kwargs):

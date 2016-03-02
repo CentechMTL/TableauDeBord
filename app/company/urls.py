b87371d3e8e64f4bedb00770d2670b912cd45309
Nicholas Savard <nicholas.savard@etsmtl.ca>
2016-03-02 15:17:38 -0500

    Added CRUD views and forms for rental

--- app/company/urls.py
+++ app/company/urls.py
@@ -25,4 +25,10 @@ urlpatterns = patterns(
     url(r'presence/add/$', views.PresenceAdd.as_view(), name='presence_add'),
     url(r'presence/update/(?P<pk>\d+)/$', views.PresenceUpdate.as_view(), name='presence_update'),
     url(r'presence/delete/(?P<pk>\d+)/$', views.PresenceDelete.as_view(), name='presence_delete'),
+
+    # System of rental
+    url(r'^rental/add$', views.RentalCreate.as_view(), name='rental_create'),
+    url(r'^rental/add/(?P<pk>\d+)/$', views.RentalCreate.as_view(), name='rental_create'),
+    url(r'^rental/update/(?P<pk>\d+)/$', views.RentalUpdate.as_view(), name='rental_update'),
+    url(r'^rental/delete/(?P<pk>\d+)/$', views.RentalDelete.as_view(), name='rental_delete'),
 )

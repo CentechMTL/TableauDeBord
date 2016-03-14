fd2a6677b406326cfd672f5e4188ca3e6c0df376
RignonNoel <rignon.noel@openmailbox.org>
2016-03-14 12:32:14 -0400

    fix pep8 on experiment app

--- app/experiment/urls.py
+++ app/experiment/urls.py
@@ -1,14 +1,32 @@
 # coding: utf-8
 
-from django.conf.urls import patterns, url,include
+from django.conf.urls import patterns, url, include
 from app.experiment import views
 
-urlpatterns = patterns('',
-    #Display all experiment of a company
-    url(r'^(\d+)$', views.CustomerExperimentList.as_view(), name='experiment_list'),
+urlpatterns = patterns(
+    '',
 
-    #Create, update, delete
-    url(r'add/(\d+)/$', views.CustomerExperimentCreate.as_view(), name='experiment_add'),
-    url(r'update/(?P<pk>\d+)/$', views.CustomerExperimentUpdate.as_view(), name='experiment_update'),
-    url(r'delete/(?P<pk>\d+)/$', views.CustomerExperimentDelete.as_view(), name='experiment_delete'),
+    # Display all experiment of a company
+    url(
+        r'^(\d+)$',
+        views.CustomerExperimentList.as_view(),
+        name='experiment_list'
+    ),
+
+    # Create, update, delete
+    url(
+        r'add/(\d+)/$',
+        views.CustomerExperimentCreate.as_view(),
+        name='experiment_add'
+    ),
+    url(
+        r'update/(?P<pk>\d+)/$',
+        views.CustomerExperimentUpdate.as_view(),
+        name='experiment_update'
+    ),
+    url(
+        r'delete/(?P<pk>\d+)/$',
+        views.CustomerExperimentDelete.as_view(),
+        name='experiment_delete'
+    ),
 )

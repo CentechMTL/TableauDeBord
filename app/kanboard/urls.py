f53f8a29f194122cd93e99824e62f48dd3638568
RignonNoel <rignon.noel@openmailbox.org>
2016-03-14 14:00:09 -0400

    fix pep8 on kanboard app

--- app/kanboard/urls.py
+++ app/kanboard/urls.py
@@ -1,17 +1,51 @@
 from django.conf.urls import patterns, url, include
 from app.kanboard import views
 
-urlpatterns = patterns('app.kanboard.views',
-    #kanboard of a company
-    url(r'^(?P<pk>\d+)/$', views.BoardIndex.as_view(), name='kanboard'),
-    #Detail of a card
-    url(r'^card/(?P<pk>\d+)/$', views.CardView.as_view(), name='card'),
+urlpatterns = patterns(
+    'app.kanboard.views',
 
-    #Ajax
-    url(r'^(\d+)/update/$', views.update),
-    url(r'^(\d+)/add/$', views.addCard, name='kanboardAddCard'),
-    url(r'^addComment/(\d+)$', views.addComment, name='addComment'),
-    url(r'^delete/card/(\d+)$', views.deleteCard, name='kanboardDeleteCard'),
-    url(r'^getDetail/card/(\d+)$', views.getDetailCard, name='kanboardGetDetailCard'),
-    url(r'^getKanboard/(\d+)/([a-zA-Z]+)$', views.getDetailKanboard, name='getDetailKanboard'),
+    # kanboard of a company
+    url(
+        r'^(?P<pk>\d+)/$',
+        views.BoardIndex.as_view(),
+        name='kanboard'
+    ),
+
+    # Detail of a card
+    url(
+        r'^card/(?P<pk>\d+)/$',
+        views.CardView.as_view(),
+        name='card'
+    ),
+
+    # Ajax
+    url(
+        r'^(\d+)/update/$',
+        views.update
+    ),
+    url(
+        r'^(\d+)/add/$',
+        views.addCard,
+        name='kanboardAddCard'
+    ),
+    url(
+        r'^addComment/(\d+)$',
+        views.addComment,
+        name='addComment'
+    ),
+    url(
+        r'^delete/card/(\d+)$',
+        views.deleteCard,
+        name='kanboardDeleteCard'
+    ),
+    url(
+        r'^getDetail/card/(\d+)$',
+        views.getDetailCard,
+        name='kanboardGetDetailCard'
+    ),
+    url(
+        r'^getKanboard/(\d+)/([a-zA-Z]+)$',
+        views.getDetailKanboard,
+        name='getDetailKanboard'
+    ),
 )
--- app/kanboard/views.py
+++ app/kanboard/views.py
@@ -22,7 +22,7 @@ PHASE_RE = re.compile(u'^phase-([\d]+)\[\]$')
 CARD_RE = re.compile(u'^card-([\d]+)$')
 
 
-#Return detail of a kanboard
+# Return detail of a kanboard
 def getDetailKanboard(request, company_id, state):
     if request.is_ajax():
         message = []
@@ -36,12 +36,14 @@ def getDetailKanboard(request, company_id, state):
 
         for phaseLoop in PHASE_CHOICES:
             phase = {}
-            cards = Card.objects.filter(phase = phaseLoop[1],
-                                        company = company_id)
+            cards = Card.objects.filter(
+                phase=phaseLoop[1],
+                company=company_id
+            )
             for card in cards:
-                if(state == card.state):
+                if state == card.state:
                     phase[card.id] = card.id
-                elif(state == None):
+                elif state is None:
                     phase[card.id] = card.id
             tuple = (phaseLoop[1], phase)
             message.append(tuple)
@@ -49,10 +51,11 @@ def getDetailKanboard(request, company_id, state):
         data = json.dumps(message)
         return HttpResponse(data, content_type='application/json')
 
-    #The visitor can't see this page!
+    # The visitor can't see this page!
     return HttpResponseRedirect("/user/noAccessPermissions")
 
-#Return detail of a card
+
+# Return detail of a card
 def getDetailCard(request, card_id):
     message = {}
 
@@ -71,11 +74,11 @@ def getDetailCard(request, card_id):
             message['id'] = card.id
             message['creator'] = card.creator.username
 
-            if card.assigned :
+            if card.assigned:
                 message['assigned'] = card.assigned.userProfile_id
                 message['picture'] = str(card.assigned.picture)
 
-            if card.deadline :
+            if card.deadline:
                 message['deadline'] = card.deadline.strftime('%Y-%m-%d')
 
         except:
@@ -83,22 +86,23 @@ def getDetailCard(request, card_id):
         data = json.dumps(message)
         return HttpResponse(data, content_type='application/json')
 
-    #The visitor can't see this page!
+    # The visitor can't see this page!
     return HttpResponseRedirect("/user/noAccessPermissions")
 
-#Add a card
+
+# Add a card
 def addCard(request, id):
     if request.is_ajax():
         if request.method == "POST":
             error = False
 
-            #We take data
+            # We take data
             title = request.POST.get('title', '')
-            if title == None or title == "":
+            if title is None or title == "":
                 error = True
 
             comment = request.POST.get('comment', '')
-            if comment == None:
+            if comment is None:
                 comment = ""
 
             deadline = request.POST.get('deadline', '')
@@ -115,7 +119,7 @@ def addCard(request, id):
                     print elem[0]
                     if elem[0] == int(phase):
                         exist = True
-                if exist == False:
+                if not exist:
                     error = True
             except:
                 error = True
@@ -126,50 +130,54 @@ def addCard(request, id):
             else:
                 state = True
 
-            #If we have all data
-            if error == False:
-                #If it's a new card
+            # If we have all data
+            if not error:
+                # If it's a new card
                 if(update == 'False'):
                     pictureAssigned = False
                     phase = PHASE_CHOICES[phase-1]
-                    company = Company.objects.get(id = id)
+                    company = Company.objects.get(id=id)
 
-                    card = Card(title = title,
-                                company = company,
-                                comment = comment,
-                                creator = request.user,
-                                phase = phase[1],
-                                order = order,
-                                state = state
-                                )
+                    card = Card(
+                        title=title,
+                        company=company,
+                        comment=comment,
+                        creator=request.user,
+                        phase=phase[1],
+                        order=order,
+                        state=state
+                    )
 
                     if deadline != "":
                         card.deadline = deadline
                     if assigned != "":
-                        founder = Founder.objects.get(userProfile_id = assigned)
+                        founder = Founder.objects.get(userProfile_id=assigned)
                         card.assigned = founder
                         pictureAssigned = str(founder.picture)
                     else:
                         card.assigned = None
                     card.save()
                     id = card.id
-                    return JsonResponse({'phase': phase[0],
-                                         'company': company.id,
-                                         'id': id,
-                                         'title': title,
-                                         'comment': comment,
-                                         'updated': False,
-                                         'picture': pictureAssigned,
-                                         'assigned': assigned,
-                                         })
+                    return JsonResponse(
+                        {
+                            'phase': phase[0],
+                            'company': company.id,
+                            'id': id,
+                            'title': title,
+                            'comment': comment,
+                            'updated': False,
+                            'picture': pictureAssigned,
+                            'assigned': assigned,
+                        }
+                    )
 
-                #If it's an update of a card
+                # If it's an update of a card
                 else:
                     pictureAssigned = False
                     phase = PHASE_CHOICES[phase-1]
-                    company = Company.objects.get(id = id)
+                    company = Company.objects.get(id=id)
 
-                    card = Card.objects.get(id = update)
+                    card = Card.objects.get(id=update)
                     card.title = title
                     card.state = state
                     card.comment = comment
@@ -180,7 +188,7 @@ def addCard(request, id):
                     else:
                         card.deadline = None
                     if assigned != "":
-                        founder = Founder.objects.get(userProfile_id = assigned)
+                        founder = Founder.objects.get(userProfile_id=assigned)
                         card.assigned = founder
                         pictureAssigned = str(founder.picture)
                     else:
@@ -196,54 +204,60 @@ def addCard(request, id):
                                          'assigned': assigned
                                          })
 
-    #The visitor can't see this page!
+    # The visitor can't see this page!
     return HttpResponseRedirect("/user/noAccessPermissions")
 
-#Delete a card of the kanboard
+
+# Delete a card of the kanboard
 def deleteCard(request, card_id):
-    message= {}
+    message = {}
 
     if request.is_ajax():
-        card = Card.objects.get(id = card_id)
+        card = Card.objects.get(id=card_id)
         card.delete()
         message['delete'] = "Deleted"
 
         data = json.dumps(message)
         return HttpResponse(data, content_type='application/json')
-    #The visitor can't see this page!
+    # The visitor can't see this page!
     return HttpResponseRedirect("/user/noAccessPermissions")
 
-#Update the kanboard
+
+# Update the kanboard
 def update(request, id):
     updates = []
     if request.is_ajax():
         try:
-            company = Company.objects.get(id = id)
+            company = Company.objects.get(id=id)
 
-            #Verify data and create a tmp list of cards and phases
+            # Verify data and create a tmp list of cards and phases
             for phase_name, card_names in request.POST.lists():
                 cards = []
-                phase_match = PHASE_RE.match(phase_name) #Regex to verify data
+                # Regex to verify data
+                phase_match = PHASE_RE.match(phase_name)
                 if not phase_match:
                     raise Exception("Malformed phase_name: <%s>" % phase_name)
                 phase = PHASE_CHOICES[int(phase_match.group(1))-1]
 
-                #Listing of cards in the phase
+                # Listing of cards in the phase
                 for card_name in card_names:
-                    card_match = CARD_RE.match(card_name) #Regex to verify data
+                    # Regex to verify data
+                    card_match = CARD_RE.match(card_name)
                     if not card_match:
-                        raise Exception("Malformed card_name: <%s>" % card_name)
+                        raise Exception(
+                            "Malformed card_name: <%s>" % card_name
+                        )
                     card = get_object_or_404(Card, pk=int(card_match.group(1)))
                     cards.append(card)
                 updates.append((phase[1], cards))
 
-                #Update phase of cards
+                # Update phase of cards
                 for phase, cards in updates:
                     for card in cards:
                         if card.phase != phase:
                             card.change_phase(phase)
 
-                #Update order of cards
+                # Update order of cards
                 for phase, cards in updates:
                     for i, card in enumerate(cards):
                         card.order = i+1
@@ -253,41 +267,48 @@ def update(request, id):
             print "Exception: %s, %r" % (e, e)
             raise
 
-    return http.HttpResponse() # nothing exciting...
+    return http.HttpResponse()  # nothing exciting...
 
-#Add a card
+
+# Add a card
 def addComment(request, id):
     if request.is_ajax():
         if request.method == "POST":
             error = False
 
-            #We take data
+            # We take data
             comment = request.POST.get('comment', '')
-            if comment == None or comment == "":
+            if comment is None or comment == "":
                 error = True
 
-            #If we have all data
-            if error == False:
-                comment = Comment(comment = comment,
-                            card = Card.objects.get(id=id),
-                            creator = request.user
-                            )
+            # If we have all data
+            if not error:
+                comment = Comment(
+                    comment=comment,
+                    card=Card.objects.get(id=id),
+                    creator=request.user
+                )
 
                 comment.save()
 
-                return JsonResponse({'comment': comment.comment,
-                                     'creator': comment.creator.username,
-                                     'created': comment.created.strftime("%Y/%m/%d"),
-                                     'id': comment.id,
-                                     })
+                return JsonResponse(
+                    {
+                        'comment': comment.comment,
+                        'creator': comment.creator.username,
+                        'created': comment.created.strftime("%Y/%m/%d"),
+                        'id': comment.id,
+                    }
+                )
+
 
 class BoardIndex(generic.TemplateView):
     template_name = 'kanboard/board.html'
 
-    #You need to be connected, and you need to have access as founder, mentor or Centech
+    # You need to be connected, and you need to have access
+    # as founder, mentor or Centech
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
-        company = get_object_or_404(Company, id = int(kwargs['pk']))
+        company = get_object_or_404(Company, id=int(kwargs['pk']))
 
         if self.request.user.profile.isCentech():
             return super(BoardIndex, self).dispatch(*args, **kwargs)
@@ -300,11 +321,11 @@ class BoardIndex(generic.TemplateView):
             if company in self.request.user.profile.isMentor().company.all():
                 return super(BoardIndex, self).dispatch(*args, **kwargs)
 
-        #The visitor can't see this page!
+        # The visitor can't see this page!
         return HttpResponseRedirect("/user/noAccessPermissions")
 
     def get_context_data(self, **kwargs):
-        company = Company.objects.get(id = int(kwargs['pk']))
+        company = Company.objects.get(id=int(kwargs['pk']))
 
         context = super(BoardIndex, self).get_context_data(**kwargs)
         context['company'] = company
@@ -312,16 +333,18 @@ class BoardIndex(generic.TemplateView):
 
         return context
 
-#Display detail of the company
+
+# Display detail of the company
 class CardView(generic.DetailView):
     model = Card
     template_name = 'kanboard/card.html'
 
-        #You need to be connected, and you need to have access as founder, mentor or Centech
+    # You need to be connected, and you need to have access
+    # as founder, mentor or Centech
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
-        card = get_object_or_404(Card, id = int(kwargs['pk']))
-        company = get_object_or_404(Company, id = card.company.id)
+        card = get_object_or_404(Card, id=int(kwargs['pk']))
+        company = get_object_or_404(Company, id=card.company.id)
 
         if self.request.user.profile.isCentech():
             return super(CardView, self).dispatch(*args, **kwargs)
@@ -334,5 +357,5 @@ class CardView(generic.DetailView):
             if company in self.request.user.profile.isMentor().company.all():
                 return super(CardView, self).dispatch(*args, **kwargs)
 
-        #The visitor can't see this page!
+        # The visitor can't see this page!
         return HttpResponseRedirect("/user/noAccessPermissions")

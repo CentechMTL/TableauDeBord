7cb9f2e7f9912816f2b6fe89721201a6daa68d8c
RignonNoel <rignon.noel@openmailbox.org>
2016-03-17 16:58:34 -0400

    remove url of presence

--- app/company/urls.py
+++ app/company/urls.py
@@ -46,33 +46,6 @@ urlpatterns = patterns(
         name='status_create'
     ),
 
-    # System of presence
-    url(
-        r'^presence/$',
-        views.PresenceList.as_view(),
-        name='presence_list'
-    ),
-    url(
-        r'^presence/(?P<status>\d+)/$',
-        views.PresenceList.as_view(),
-        name='presence_list'
-    ),
-    url(
-        r'presence/add/$',
-        views.PresenceAdd.as_view(),
-        name='presence_add'
-    ),
-    url(
-        r'presence/update/(?P<pk>\d+)/$',
-        views.PresenceUpdate.as_view(),
-        name='presence_update'
-    ),
-    url(
-        r'presence/delete/(?P<pk>\d+)/$',
-        views.PresenceDelete.as_view(),
-        name='presence_delete'
-    ),
-
     # System of rental
     url(
         r'^rental/add$',

5f24e55acd010ac39270d26ad6093f7f5dcd3c60
Nicholas Savard <nicholas.savard@etsmtl.ca>
2016-04-06 16:44:13 -0400

    Added right access tests

--- app/floorMap/tests/tests_views.py
+++ app/floorMap/tests/tests_views.py
@@ -101,3 +101,82 @@ class FloorMapTest (TestCase):
             follow=False
         )
         self.assertEqual(result.status_code, 302)
+
+    def test_room_details(self):
+        """
+        To test the display of the room details
+        """
+
+        """
+        Access : Staff
+        """
+        self.client.logout()
+        self.client.login(
+            username=self.staff.user.username,
+            password="Toto1234!#"
+        )
+
+        result = self.client.get(
+            reverse('floorMap:index'),
+            follow=False
+        )
+        self.assertEqual(result.status_code, 200)
+
+        """
+        Access : Founder
+        """
+        self.client.logout()
+        self.client.login(
+            username=self.founder.user.username,
+            password="Toto1234!#"
+        )
+
+        result = self.client.get(
+            reverse('floorMap:index'),
+            follow=False
+        )
+        self.assertEqual(result.status_code, 200)
+
+        """
+        Access : Mentor
+        """
+        self.client.logout()
+        self.client.login(
+            username=self.mentor.user.username,
+            password="Toto1234!#"
+        )
+
+        result = self.client.get(
+            reverse('floorMap:index'),
+            follow=False
+        )
+        self.assertEqual(result.status_code, 200)
+
+        """
+        Access : Executive
+        """
+        self.client.logout()
+        self.client.login(
+            username=self.executive.user.username,
+            password="Toto1234!#"
+        )
+
+        result = self.client.get(
+            reverse('floorMap:index'),
+            follow=False
+        )
+        self.assertEqual(result.status_code, 200)
+
+        """
+        No Access : Not logged
+        """
+        self.client.logout()
+
+        result = self.client.get(
+            reverse(
+                'founder:detail', kwargs={
+                    'pk': self.founder.userProfile_id
+                }),
+            follow=False
+        )
+        self.assertEqual(result.status_code, 302)
